7e441576865f43d8a67144fc6db01e7405316bc6
pmayur <thisispatil@gmail.com>
2019-05-01 05:51:14 +0530

    working get and post requests from the front end

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 export default class CreateTodo extends Component {
-    render() {
+      
+    /* render() {
         return (
             <div>
                 <p>Welcome to Create Todo Component!!</p>
